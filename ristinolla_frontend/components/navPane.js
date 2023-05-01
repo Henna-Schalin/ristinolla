ba07fa024e202331c761a5369e9aee2635c4cbf4
Henna-Schalin <132266103+Henna-Schalin@users.noreply.github.com>
2023-05-01 19:19:04 +0300

    Add files via upload

--- ristinolla_frontend/components/navPane.js
+++ ristinolla_frontend/components/navPane.js
@@ -38,16 +38,18 @@ const NavPane = () => {
 
   return (
     <>
-      <Grid sx={{ height: '400px', display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
+    <Grid container sx={{ position: 'absolute', left: 0, top: 0}}>
+      <Grid sx={{ height: '400px', display: 'flex', justifyContent: 'center', alignItems: 'flex-end', width: '100%' }}>
         <MyModal modalOpen={modalOpen} handleClose={handleClose} style={{ zIndex: 2000 }} modalType={modalType} />
-        <SettingsOutlinedIcon onClick={() => handleClick('settingsModal')} sx={{ fontSize: '70px', marginRight: '5px', marginLeft: '10px', zIndex: 1000 }} />
-        <ScoreboardOutlinedIcon onClick={() => handleClick('highScore')} sx={{ fontSize: '70px', marginRight: '5px', marginLeft: '10px', zIndex: 1000 }} />
-        <ListIcon onClick={() => handleClick('gameList')} sx={{ fontSize: '70px', marginRight: '5px', marginLeft: '10px', zIndex: 1000 }} />
+        <SettingsOutlinedIcon onClick={() => handleClick('settingsModal')} sx={{ fontSize: '70px',  marginLeft: '10px', zIndex: 1000, cursor: 'pointer'}} />
+        <ScoreboardOutlinedIcon onClick={() => handleClick('highScore')} sx={{ fontSize: '70px', marginLeft: '10px', zIndex: 1000, cursor: 'pointer'}} />
+        <ListIcon onClick={() => handleClick('gameList')} sx={{ fontSize: '70px', marginLeft: '10px', zIndex: 1000, cursor: 'pointer' }} />
       </Grid>
       <Grid container>
-        <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+        <Grid item xs={12} style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', width: '99%' }}>
           {showPrompt && <p className="prompt">Open the scroll to begin</p>}
         </Grid>
+      </Grid>
       </Grid>
     </>
   )
